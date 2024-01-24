fb3146f44a8819219c2d00fd9259e40762144c2f
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-01-24 23:16:27 +0300

    Update dbConfig.js

--- src/dbConfig/dbConfig.js
+++ src/dbConfig/dbConfig.js
@@ -2,21 +2,21 @@ import mongoose from "mongoose";
 
 export default async function connect() {
   try {
-  await  mongoose.connect(process.env.MONGO_URL!);
-      const connection = mongoose.connection;
- 
-      connection.on('connected', () => {
-     console.log('MongoDB connected successfully')
-      })
+    await mongoose.connect(process.env.MONGO_URL);
+    const connection = mongoose.connection;
 
-      connection.on('error', (err) => {
-          console.log('MongoDB connection error. Please make sure MongoDB is running.' + err);
-          process.exit();
-      })
+    connection.on("open", () => {
+      console.log("MongoDB connected successfully");
+    });
 
+    connection.on("error", (err) => {
+      console.log(
+        "MongoDB connection error. Please make sure MongoDB is running." + err
+      );
+      process.exit();
+    });
   } catch (error) {
     console.log("Error: Something went wrong!");
     console.log(error);
   }
 }
-
