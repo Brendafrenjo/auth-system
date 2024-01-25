8b534a60b0b11b1b389f146d986eb99e0a283038
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-01-25 10:25:38 +0300

    Update page.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,7 +1,70 @@
-export default function LogInPage() {
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../page.module.css";
+
+const LoginPage = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  async function onLogin() {}
+
+  function handleChange(e) {
+    const { value, name } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  }
+
   return (
-    <div>
-      <h1>Log In Page</h1>
+    <div className="container signup-form">
+      <h1 className="text-center mt-5">Log in</h1>
+      <hr />
+      <form className="">
+        <label htmlFor="emial" id="email" className="mb-2 mt-2">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="email form-control"
+          placeholder="Email"
+          onChange={handleChange}
+          value={user.email}
+        />
+        <label
+          htmlFor="password"
+          id="password"
+          className="mb-2 mt-2 text-center"
+        >
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="password form-control"
+          placeholder="Password"
+          onChange={handleChange}
+          value={user.password}
+        />
+        <button
+          className="btn btn-success mt-3 mb-2 form-control"
+          onClick={onLogin}
+        >
+          Login here
+        </button>
+        <Link href="/signup" className="text-center">
+          Visit signup page
+        </Link>
+      </form>
     </div>
   );
-}
+};
+
+export default LoginPage;
