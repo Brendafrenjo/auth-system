daa4421f413cfbae1300e39813e1eb0814693b8c
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-01-27 09:58:47 +0300

    Create try.js
    For eventModel.js

---
+++ src/app/api/users/signup/try.js
@@ -0,0 +1,43 @@
+import { connect } from "@/dbConfig/dbConfig";
+import Event from "@/models/eventModel";
+import { NextResponse, NextRequest } from "next/server";
+import bcryptjs from "bcryptjs";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.jscon();
+    const { cityname, cityemail, citypassword } = reqBody;
+
+    //check if event exists
+    const event = await Event.findOne({ cityemail });
+
+    if (event) {
+      return NextResponse.json(
+        { error: "Event already exists" },
+        { status: 400 }
+      );
+    }
+
+    //hash password
+    const salt = await bcryptjs.genSalt(10);
+    const hashPassword = await bcrypt.hash(citypassword, salt);
+
+    const newEvent = new Event({
+      cityname,
+      cityemail,
+      citypassword: hashPassword,
+    });
+
+    const eventSaved = newEvent.save();
+
+    return NextResponse.json({
+      message: "Event created succesfully",
+      success: true,
+      eventSaved,
+    });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
